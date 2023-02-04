58a547c5e5bc2dfe1a8a5d5153df33c3a66046d7
sanumuhammedc <sanumuhammed.c@gmail.com>
2023-02-04 20:30:22 +0530

    done

---
+++ todo/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+# Create your models here.
+
+class Task(models.Model):
+    user = models.CharField(max_length=200, null=True, blank=True)
+    title = models.CharField(max_length=200, null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(default=timezone.now)
+    deadline = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created']
